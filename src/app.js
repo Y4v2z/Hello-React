d7b1e0ea02fb7e7fa0f24493e575152b1852ebb1
Yavuz Avci <yavuzavci01@gmail.com>
2023-02-13 18:05:00 +0200

    npm, babel and babelconfig installed

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// var template = React.createElement("h1", null, "My first react app.");
+// var root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(template);
+
+
+var template = <h1>My first react app.</h1>; //JSX: js Xml (jsx sayesinde etiketler içinde yazabiliriz.)
+var root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(template);
